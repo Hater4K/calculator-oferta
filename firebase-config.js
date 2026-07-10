4efe137cf84fd424941c6d1f17d8877050c31d75
Hater4K <alexandru.zaharia@connecteximp.com>
2026-07-10 23:12:38 +0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,13 @@
-﻿// Completeaza acest fisier cu configuratia din Firebase Console -> Project settings -> Your apps -> Web app.
-// Dupa ce il completezi si il urci pe GitHub langa index.html, partidele se vor salva in Firestore.
+﻿// Configuratie Firebase pentru Calculator Oferta.
+// Nu pune aici importurile Firebase; index.html le incarca separat.
 window.CALCULATOR_OFERTA_FIREBASE_CONFIG = {
-  apiKey: "PUNE_API_KEY_AICI",
-  authDomain: "PUNE_PROJECT_ID.firebaseapp.com",
-  projectId: "PUNE_PROJECT_ID",
-  storageBucket: "PUNE_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "PUNE_MESSAGING_SENDER_ID",
-  appId: "PUNE_APP_ID"
+  apiKey: "AIzaSyBpOWszvJ7J4T3JblAvx7_dkRSA6jJ2uHA",
+  authDomain: "offer-manager-d67dd.firebaseapp.com",
+  projectId: "offer-manager-d67dd",
+  storageBucket: "offer-manager-d67dd.firebasestorage.app",
+  messagingSenderId: "933692838285",
+  appId: "1:933692838285:web:67a2bb52d27434a35e2dc4",
+  measurementId: "G-5Z1P1SB576"
 };
 
 // Toate dispozitivele care folosesc acelasi document vad aceleasi partide.
